39f49ba2ccd7e2c0be803b2a8efe5b07f77e60d5
davila_jairo <jairodavila66@gmail.com>
2023-03-26 14:15:50 -0300

    modifique parametros del juego js

--- main.js
+++ main.js
@@ -1,42 +1,63 @@
-//BIENVENIDOS AL JUEGO DE ADIVINA EL NUMERO
 
-// Generamos un número aleatorio entre 1 y 15
-const numeroAleatorio = Math.floor(Math.random() * 15) + 1;
+// Recopilamos datos del usuario, para su registro
+alert (" Registrate y Que Inicie el Juego");
+let nombre   = prompt ("Ingrese su nombre");
+let apellido = prompt ("Ingrese su apellido");
+let edad     = prompt ("Ingrese su edad");
+let mail     = prompt ("Ingrese su mail");
+alert ( "DATOS USUARIO"+ "\nsu nombre es   : "+ nombre +"\nsu apellido es   : "
+    +apellido +"\nsu edad es  : " +edad +"\nsu mail es   : "+ mail);
+alert("Bienvenido, Juguemos!!");
+alert("Debes adivinar el numero con la menor cantidad de intentos");
 
-// Le pedimos al usuario que adivine el número
-let intentos = 1;
 
-let bienvenido = "Hola Te Estaba Esperando, Deseas Jugar??";
-    alert(bienvenido)
+// Genera un número aleatorio entre 1 y 100
+const numeroAleatorio = Math.floor(Math.random() * 20) + 1;
 
-// Le damos algunas  instrucciones al  usuario 
-let instrucciones= "Debes adivinar el numero con la menor cantidad de Intentos";
-    alert(instrucciones)
-let numeroUsuario = parseInt(prompt("Adivina el número (entre 1 y 15):"));
+// Establece el número de Vidas y el número máximo de Vidas permitidos
+let intentos = 0;
+const Vidas = 3;
 
-// Mientras el usuario no adivine el número, seguimos pidiéndole que lo intente
-while (numeroUsuario !== numeroAleatorio) {
-
-    if (numeroUsuario > numeroAleatorio) {
-    alert("El número es más bajo");
-
-    } else {
-    alert("El número es más alto");
+// Función para validar la entrada del usuario
+function validarEntrada(numero) {
+    if (isNaN(numero) || numero < 1 || numero > 20) {
+        alert("Ingresa un número entre 1 y 20");
+    return false;
     }
-
-    intentos++;
-    numeroUsuario = parseInt(prompt("Inténtalo de nuevo:"));
-    break
-
+    return true;
 }
 
-// Si el usuario adivina el número, mostramos un mensaje de felicitación
-alert(`¡Felicidades, adivinaste el número en ${intentos} intentos!`);
+while (intentos < Vidas) {
+  // Le pedimos al usuario que adivine el número
+    const numeroUsuario = parseInt(prompt("Adivina un número entre 1 y 20"));
 
-//si el usuarido desea seguir le sugerimos volver a cargar la pagina
+  // Verifica si la entrada del usuario es válida
+    if (validarEntrada(numeroUsuario)) {
+    // Aumenta el número de intentos
+    intentos++;
+
+    // Compara el número del usuario con el número aleatorio
+    if (numeroUsuario === numeroAleatorio) {
+        alert(`¡Felicidades ${nombre,apellido}, adivinaste el número en ${intentos} intentos!   ٩(^‿^)۶`);
+        break;
+        } else if (numeroUsuario < numeroAleatorio) {
+        alert("El número es mayor");
+        } else {
+        alert("El número es menor");
+    }
+}
+}
+
+// Si el usuario no adivina el número en el número máximo de intentos permitidos, muestra un mensaje de fracaso.
+if (intentos === Vidas) {
+    alert(`Te haz quedado sin vidas -${Vidas} \nEl número era ${numeroAleatorio}.(✖╭╮✖) `);
+}
 alert("Presiona F5 si quieres volver a intentarlo");
 
-alert("Esta bien, Vuelve pronto!!!!");
+
+
+
+
 
 
 
