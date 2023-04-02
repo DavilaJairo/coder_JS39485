1c14469f6b264078b96e6d927ff6052663b6d8fb
davila_jairo <jairodavila66@gmail.com>
2023-04-02 16:19:27 -0300

    agregue funcion registro

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-
 // Recopilamos datos del usuario, para su registro
-alert (" Registrate y Que Inicie el Juego");
-let nombre   = prompt ("Ingrese su nombre");
-let apellido = prompt ("Ingrese su apellido");
-let edad     = prompt ("Ingrese su edad");
-let mail     = prompt ("Ingrese su mail");
-alert ( "DATOS USUARIO"+ "\nsu nombre es   : "+ nombre +"\nsu apellido es   : "
-    +apellido +"\nsu edad es  : " +edad +"\nsu mail es   : "+ mail);
-alert("Bienvenido, Juguemos!!");
+function registrarUsuario() {
+    alert("¡Bienvenido/a! Por favor, ingrese sus datos:");
+
+    const usuario = {};
+
+    usuario.nombre = prompt("Nombre:");
+    usuario.apellido = prompt("Apellido:");
+    usuario.edad = parseInt(prompt("Edad:"));
+    usuario.email = prompt("Correo electrónico:");
+
+    alert("¡Gracias! Estos son tus datos:\n" +
+        "Nombre: " + usuario.nombre + "\n" +
+        "Apellido: " + usuario.apellido + "\n" +
+        "Edad: " + usuario.edad + "\n" +
+        "Correo electrónico: " + usuario.email);
+    alert("Bienvenido, Juguemos!!");
+}
+
+registrarUsuario();
+
 alert("Debes adivinar el numero con la menor cantidad de intentos");
-
-
 // Genera un número aleatorio entre 1 y 100
 const numeroAleatorio = Math.floor(Math.random() * 20) + 1;
 
@@ -50,7 +59,7 @@ while (intentos < Vidas) {
 
 // Si el usuario no adivina el número en el número máximo de intentos permitidos, muestra un mensaje de fracaso.
 if (intentos === Vidas) {
-    alert(`Te haz quedado sin vidas -${Vidas} \nEl número era ${numeroAleatorio}.(✖╭╮✖) `);
+    alert(`Te haz quedado sin vidas -${Vidas} \nEl número era ${numeroAleatorio}     (✖╭╮✖) `);
 }
 alert("Presiona F5 si quieres volver a intentarlo");
 
